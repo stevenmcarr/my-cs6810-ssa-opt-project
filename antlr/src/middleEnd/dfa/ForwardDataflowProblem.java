a7430ed4087313ecc5eb89f5d06675df3ef9c9cb
Steve Carr <steve.carr@wmich.edu>
2022-01-06 11:08:44 -0500

    abstract out data flow problem type

---
+++ antlr/src/middleEnd/dfa/ForwardDataflowProblem.java
@@ -0,0 +1,32 @@
+package middleEnd.dfa;
+
+import java.util.List;
+
+import middleEnd.utils.BasicBlock;
+import middleEnd.utils.Cfg;
+import middleEnd.utils.CfgNode;
+import middleEnd.utils.DataFlowSet;
+
+public abstract class ForwardDataflowProblem extends IterativeFramework {
+
+	@Override
+	public DataFlowSet getCurrentMeetResult(BasicBlock n) {
+		return inMap.get(n);
+	}
+
+	@Override
+	public void setMeetResult(BasicBlock n, DataFlowSet vrs) {
+		inMap.put(n, vrs);
+	}
+
+	@Override
+	public void setTransferResult(BasicBlock n, DataFlowSet vrs) {
+		outMap.put(n, vrs);
+	}
+
+	@Override
+	public List<CfgNode> getNodeOrder(Cfg g) {
+		return g.getPreOrder();
+	}
+
+}
